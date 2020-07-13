f6814b24cb61a1607001bdec373b50ad9fee66b1
Avani Patel <anpatel36@yahoo.in>
2020-07-13 19:56:55 -0400

    [AP] ids bound using ButterKnife in MainActivity

--- app/src/main/java/com/example/note_teampurple_android/Activities/MainActivity.java
+++ app/src/main/java/com/example/note_teampurple_android/Activities/MainActivity.java
@@ -1,16 +1,51 @@
 package com.example.note_teampurple_android.Activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.note_teampurple_android.Adapter.HomeAdapter;
 import com.example.note_teampurple_android.R;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.github.yavski.fabspeeddial.FabSpeedDial;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    HomeAdapter homeAdapter;
+    LinearLayoutManager layoutManager;
+    RecyclerView recyclerView;
+
+    @BindView(R.id.txt_hometitle)
+    TextView txtHometitle;
+    @BindView(R.id.edt_searchnote)
+    EditText edtSearchnote;
+    @BindView(R.id.img_sort)
+    ImageView imgSort;
+    @BindView(R.id.ll_emptynote)
+    LinearLayout llEmptynote;
+    @BindView(R.id.my_recycler_view)
+    RecyclerView myRecyclerView;
+    @BindView(R.id.fab_add)
+    FabSpeedDial fabAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
