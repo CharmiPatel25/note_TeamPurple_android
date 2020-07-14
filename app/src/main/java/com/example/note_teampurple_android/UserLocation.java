45bedcb94ddda13ee21cce7d8fcc72d863be7e93
Charmi <c0768448@mylambton.ca>
2020-07-14 19:52:14 -0400

    [cp] app executor class

--- app/src/main/java/com/example/note_teampurple_android/UserLocation.java
+++ app/src/main/java/com/example/note_teampurple_android/UserLocation.java
@@ -9,6 +9,6 @@ public class UserLocation extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_location);
+        setContentView(R.layout.activity_userlocation);
     }
 }
