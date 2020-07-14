1ef7c184df9ef5e1cfa7bdd07924f488dae96f1e
Jyothithomas <jyothithomas.05@gmail.com>
2020-07-13 21:17:32 -0400

    [JT]Interface added

---
+++ app/src/main/java/com/example/note_teampurple_android/database/AppExecutors.java
@@ -0,0 +1,4 @@
+package com.example.note_teampurple_android.database;
+
+public class AppExecutors {
+}
---
+++ app/src/main/java/com/example/note_teampurple_android/database/NoteDao.java
@@ -0,0 +1,29 @@
+package com.example.note_teampurple_android.database;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.note_teampurple_android.models.UserData;
+
+import java.util.List;
+
+@Dao
+public interface NoteDao {
+
+    @Query("SELECT * FROM Usernotes ORDER BY ID")
+    List<UserData> loadAllPersons();
+
+    @Insert
+    void insertPerson(UserData userData);
+
+    @Update
+    void updatePerson(UserData userData);
+
+    @Delete
+    void delete(UserData userData);
+
+    @Query("SELECT * FROM Usernotes WHERE id = :id")
+    UserData loadPersonById(int id);
+}
