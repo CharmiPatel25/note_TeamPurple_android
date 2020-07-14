435bfcbf540a8f8715372c0cc55a54530db25135
Jyothithomas <jyothithomas.05@gmail.com>
2020-07-13 21:06:23 -0400

    [JT]DataParser Added

--- app/src/main/java/com/example/note_teampurple_android/utils/DataParser.java
+++ app/src/main/java/com/example/note_teampurple_android/utils/DataParser.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 import java.util.List;
 
 public class DataParser {
-    
+
     private HashMap<String, String> getPlace(JSONObject googlePlaceJson) {
         HashMap<String, String> googlePlaceMap = new HashMap<>();
         String placeName = "-NA-";
