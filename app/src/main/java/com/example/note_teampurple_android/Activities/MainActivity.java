9c1c182c7e18720de14768c96c2c5fc3e9d1fc86
Charmi <c0768448@mylambton.ca>
2020-07-17 14:43:21 -0400

    [cp] resolve null pointer exception error

--- app/src/main/java/com/example/note_teampurple_android/Activities/MainActivity.java
+++ app/src/main/java/com/example/note_teampurple_android/Activities/MainActivity.java
@@ -102,7 +102,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_dashboard);
         ButterKnife.bind(this);
         userData = new UserData();
         mDb = AppDatabase.getInstance(getApplicationContext());
